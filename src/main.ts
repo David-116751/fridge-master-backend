22fe042e934ee7f907e5b3507d0939ad27a81a36
David Sun <davidsun10020413@gmail.com>
2023-02-05 13:57:20 +0800

    Setup project with required package and related files

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import express from 'express';
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+const port = 4201;
+
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+    response.on('error', (err) => {
+        console.error(err);
+    });
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello world!');
+});
+
+server.listen(port);
